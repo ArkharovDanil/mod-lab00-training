bacbe468d2cb693ffc6fd4e6d7babae65a65414b
ArkharovDanil <99728665+ArkharovDanil@users.noreply.github.com>
2022-03-05 09:03:15 +0300

    Update fun.cpp

--- src/fun.cpp
+++ src/fun.cpp
@@ -1,10 +1,6 @@
-// Copyright 2022 UNN-IASR
 #include "fun.h"
-#include <iostream>
 #include <cmath>
 
-int64_t power(int64_t x, uint16_t n)
-{
-  int ans=  pow(x,n);
-  return ans;
+int64_t power(int64_t x, uint16_t n) {
+    return pow(x, n);
 }
